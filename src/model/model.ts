1922b526b2da9b5548685cc834d8408052b5657d
ridhav30 <ridha30@gmail.com>
2023-12-14 16:53:34 +0530

    added component models

--- src/model/model.ts
+++ src/model/model.ts
@@ -1,8 +1,11 @@
 import {
-    User,TUser,
-    Address,TAddress
+    User, TUser,
+    Address, TAddress
 } from './authModel'
+import { Nav, NavData, TNav, TNavData } from './componentModel'
 export {
-    User,TUser,
-    Address,TAddress
+    User, TUser,
+    Address, TAddress,
+    Nav, NavData, TNav,
+    TNavData
 }
