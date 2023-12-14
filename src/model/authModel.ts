bcde2731e64fc567b28a2f00ff29a1bb37644159
ridhav30 <ridha30@gmail.com>
2023-12-14 09:29:40 +0530

    added addres with user

--- src/model/authModel.ts
+++ src/model/authModel.ts
@@ -1,6 +1,9 @@
 import { DataTypes } from 'sequelize'
 import { sequelize } from '../db/db'
-
+const { INTEGER, STRING } = DataTypes
+/*
+    User Table
+*/
 export type TUser = {
     userId?: number;
     firstName?: string;
@@ -11,26 +14,79 @@ export type TUser = {
 }
 export const User = sequelize.define<any, TUser>('Users', {
     userId: {
-        type: DataTypes.INTEGER,
+        type: INTEGER,
         autoIncrement: true,
         primaryKey: true,
         allowNull: false
     },
     firstName: {
-        type: DataTypes.STRING,
+        type: STRING,
         allowNull: false
     },
     lastName: {
-        type: DataTypes.STRING,
+        type: STRING,
         allowNull: false
     },
     email: {
-        type: DataTypes.STRING,
+        type: STRING,
         allowNull: false
     },
     password: {
-        type: DataTypes.STRING,
+        type: STRING,
         allowNull: false
     }
 })
 
+/*
+    Address Table
+*/
+
+export type TAddress = {
+    addressId?: number;
+    houseNo?: string;
+    streetName?: string;
+    address?: string;
+    city?: string;
+    state?: string;
+    country?: string;
+    userId?: number
+}
+
+export const Address = sequelize.define<any, TAddress>('Addresses', {
+    addressId: {
+        type: INTEGER,
+        autoIncrement: true,
+        primaryKey: true,
+        allowNull: false
+    },
+    houseNo: {
+        type: STRING,
+        allowNull: true,
+    },
+    address: {
+        type: STRING,
+        allowNull: false
+    },
+    streetName: {
+        type: STRING,
+        allowNull: false
+    },
+    city: {
+        type: STRING,
+        allowNull: false
+    },
+    state: {
+        type: STRING,
+        allowNull: false
+    },
+    country: {
+        type: STRING,
+        allowNull: false
+    },
+    userId: {
+        type: STRING,
+        allowNull: false
+    }
+})
+User.hasMany(Address, { foreignKey: 'userId', onDelete: 'CASCADE' })
+Address.belongsTo(User, { foreignKey: 'userId' })
