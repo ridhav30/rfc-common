86c7999f5b0ce556c0f04919e740cbbe604a847a
ridhav30 <ridha30@gmail.com>
2023-12-14 22:36:21 +0530

    removed unwanted things

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,6 @@ export * from './helpers/helper'
 export * from './model/model'
 export * from './db/db'
 export * from './constants/status'
-export type * from './model/model' 
 //import { TAddress, TNav, TUser, TNavData } from './model/model'
 //export type { TAddress, TNav, TUser, TNavData }
 
