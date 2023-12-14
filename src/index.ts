32f6380ed87ee8d11421bcbca340f898a7e075c8
ridhav30 <ridha30@gmail.com>
2023-12-14 22:31:27 +0530

    export all things

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,7 @@
-import * as helper from './helpers/helper'
-import * as model from './model/model'
-import * as db from './db/db'
-import * as constants from './constants/status'
-import { TAddress, TNav, TUser, TNavData } from './model/model'
-export type { TAddress, TNav, TUser, TNavData }
-export {
-    helper,
-    model,
-    db,
-    constants
-}
+export * as helper from './helpers/helper'
+export * as model from './model/model'
+export * as db from './db/db'
+export * as constants from './constants/status'
+//import { TAddress, TNav, TUser, TNavData } from './model/model'
+//export type { TAddress, TNav, TUser, TNavData }
+
