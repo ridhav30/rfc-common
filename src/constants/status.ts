3fc5a361ffb138e381092898cfd76b86a8452fb3
ridhav30 <ridha30@gmail.com>
2023-12-14 17:20:13 +0530

    added constants

---
+++ src/constants/status.ts
@@ -0,0 +1,10 @@
+export const status ={
+    'SUCCESS':200,
+    'SERVER_ERROR':500,
+    'NO_CONTENT':204,
+    'NOT_FOUND':404,
+    'BAD_REQUEST':400,
+    'VALIDATION_ERROR':422,
+    'UNAUTHERISED_DATA':401,
+    'FORBBIDDEN':403
+}
--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,11 @@
 import * as helper from './helpers/helper'
 import * as model from './model/model'
 import * as db from './db/db'
+import * as constants from './constants/status'
 
 export {
     helper,
     model,
-    db
+    db,
+    constants
 }
