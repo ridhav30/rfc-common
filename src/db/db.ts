a6efe3afa135ab26d955a618e0496e3753717bc4
ridhav30 <ridha30@gmail.com>
2023-12-19 10:21:03 +0530

    added sequalize authenticate

--- src/db/db.ts
+++ src/db/db.ts
@@ -1,12 +1,24 @@
-import { config } from 'dotenv'
-import { Sequelize } from 'sequelize'
-config()
-const DATABASE_NAME = process.env.DATABASE_NAME ?? ''
-const DATABASE_USER = process.env.DATABASE_USER ?? ''
-const DATABASE_PASSWORD = process.env.DATABASE_PASSWORD ?? ''
-const DB_HOST = process.env.DB_HOST ?? ''
-const DIALECT: any = process.env.DIALECT
-export const sequelize = new Sequelize(DATABASE_NAME, DATABASE_USER, DATABASE_PASSWORD, {
+import { config } from "dotenv";
+import { Sequelize } from "sequelize";
+config();
+const DATABASE_NAME = process.env.DATABASE_NAME ?? "";
+const DATABASE_USER = process.env.DATABASE_USER ?? "";
+const DATABASE_PASSWORD = process.env.DATABASE_PASSWORD ?? "";
+const DB_HOST = process.env.DB_HOST ?? "";
+const DIALECT: any = process.env.DIALECT;
+const sequelize = new Sequelize(
+  DATABASE_NAME,
+  DATABASE_USER,
+  DATABASE_PASSWORD,
+  {
     host: DB_HOST,
-    dialect: DIALECT
-})
+    dialect: DIALECT,
+  }
+);
+sequelize
+  .authenticate()
+  .then(() => {
+    console.log("db connected");
+  })
+  .catch((e) => console.log(e));
+export { sequelize };
