3fec8fe721939c476ed33dae8219e6ad281966fc
ridhav30 <ridha30@gmail.com>
2023-12-30 14:14:51 +0530

    all issue resolved

--- src/helpers/commonHelper.ts
+++ src/helpers/commonHelper.ts
@@ -1,36 +1,36 @@
-// import * as httpExpressContext from "express-http-context";
-// import { verify } from "jsonwebtoken";
-// import { NextFunction, Request, Response } from "express";
-// import { logger, responseGenerator } from "./helper";
-// import { config } from "dotenv";
-// config();
-// export const httpContext = httpExpressContext;
-// export const verfyAccessToken = (
-//   req: Request,
-//   res: Response,
-//   next: NextFunction
-// ) => {
-//   try {
-//     const sceretKey = process.env.SECRET_KEY ?? "";
-//     const token = req.get("authorization");
-//     if (!token) {
-//       return responseGenerator(res, {
-//         apiStatus: 401,
-//         responseStatus: 401,
-//         message: "Unauthorized access - Token not provided",
-//         data: [],
-//       });
-//     }
-//     const decoded = verify(token, sceretKey);
-//     httpContext.set("user", decoded);
-//     next();
-//   } catch (error) {
-//     logger.error(`jwtVerificationError:${error}`);
-//     return responseGenerator(res, {
-//       apiStatus: 401,
-//       responseStatus: 401,
-//       message: "Unauthorized access - Invalid token",
-//       data: [],
-//     });
-//   }
-// };
+import * as httpExpressContext from "express-http-context";
+import jwt from 'jsonwebtoken';
+import { NextFunction, Request, Response } from "express";
+import { logger, responseGenerator } from "./helper";
+import { config } from "dotenv";
+config();
+export const httpContext = httpExpressContext;
+export const verfyAccessToken = (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const sceretKey = process.env.SECRET_KEY ?? "";
+    const token = req.get("authorization");
+    if (!token) {
+      return responseGenerator(res, {
+        apiStatus: 401,
+        responseStatus: 401,
+        message: "Unauthorized access - Token not provided",
+        data: [],
+      });
+    }
+    const decoded = jwt.verify(token, sceretKey);
+    httpContext.set("user", decoded);
+    next();
+  } catch (error) {
+    logger.error(`jwtVerificationError:${error}`);
+    return responseGenerator(res, {
+      apiStatus: 401,
+      responseStatus: 401,
+      message: "Unauthorized access - Invalid token",
+      data: [],
+    });
+  }
+};
